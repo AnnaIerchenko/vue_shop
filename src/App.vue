d78392e36b60f20123e8443ec248670906c268c0
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-01-03 12:10:31 -0800

    remove header in own component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,12 @@
 <script setup>
-
+ import HeaderVue from './components/HeaderVue.vue'
 </script>
 
 <template>
-  <div class="bg-white h-screen w-4/5 m-auto rounded-xl shadow-xl mt-14">
-    <header class="flex justify-between border-b border-slate-300 px-14 py-8">
-      <div class="flex items-center gap-4">
-        <img src="/logo.png" alt="logo" class="w-10">
-      <div>
-        <h2 class="text-xl font-bold uppercase">Vue commerce</h2>
-        <p class="text-slate-400">Commerce the best shoes</p>
-      </div>
+    <div class="bg-white h-screen w-4/5 m-auto rounded-xl shadow-xl mt-14">
+      <header-vue />
+      <h1>About Page</h1>
     </div>
-    <ul class="flex items-center gap-10">
-      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
-        <img src="/cart.svg" alt="cart">
-        <b>128$</b>
-      </li>
-      <li class="flex items-center gap-3 text-slate-400 hover:text-black cursor-pointer">
-        <img src="/heart.svg" alt="heart">
-        <span>Favourites</span>
-      </li>
-      <li class="flex items-center gap-3 text-slate-400 hover:text-black cursor-pointer">
-        <img src="/profile.svg" alt="photo">
-        <span>Profile</span>
-      </li>
-    </ul>
-    </header>
-    <h1>About Page</h1>
-  </div>
 </template>
 
 <style scoped>
---
+++ src/components/HeaderVue.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <header class="flex justify-between border-b border-slate-300 px-14 py-8">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="logo" class="w-10">
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue commerce</h2>
+        <p class="text-slate-400">Commerce the best shoes</p>
+      </div>
+    </div>
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 text-slate-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="cart">
+        <b>128$</b>
+      </li>
+      <li class="flex items-center gap-3 text-slate-400 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="heart">
+        <span>Favourites</span>
+      </li>
+      <li class="flex items-center gap-3 text-slate-400 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="photo">
+        <span>Profile</span>
+      </li>
+    </ul>
+    </header>
+
+
+</template>
